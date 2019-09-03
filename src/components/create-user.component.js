7a6fb835f9a390d63c12fee0a2d71e8453ca5943
Colin Bowen <colinbowen@colins-mbp.lan>
2019-09-03 14:01:05 +0100

    Updated API Links

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -33,7 +33,7 @@ export default class CreateUser extends Component {
 
         console.log(user);
 
-        axios.post('http://localhost:5000/users/add', user)
+        axios.post('https://limitless-temple-61121.herokuapp.com/users/add', user)
         .then(res => console.log(res.data));
 
         this.setState({
